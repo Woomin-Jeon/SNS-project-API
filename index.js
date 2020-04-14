5b7f2819ae5e8530f61a97be07942a0a1a7c0b8d
woomin <dal96k@gmail.com>
2020-04-14 20:38:30 +0900

    For project deploying

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const logger = require('./logs/winston');
 require('./models/index');
 require('./socketio/index');
 
+const port = process.env.PORT || 3000;
+
 app.listen(process.env.PORT, () => {
-  logger.info(`server is running on port ${process.env.PORT}...`);
+  logger.info(`server is running on port ${port}...`);
 });
