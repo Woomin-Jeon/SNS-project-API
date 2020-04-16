08b51498550bcd6ed3a0f3cde0ad675c93278d54
woomin <dal96k@gmail.com>
2020-04-16 20:01:45 +0900

    deploy...

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const logger = require('../logs/winston');
 
 const db = mongoose.connection;
-db.on('error', console.error);
+db.on('error', logger.info('DB에 에러났음'));
 db.once('open', function(){
   logger.info("mongoDB is connected");
 });
@@ -11,6 +11,6 @@ mongoose.connect(process.env.MONGODB_URI, {
   useCreateIndex: true,
   useUnifiedTopology: true,
   useNewUrlParser: true,
-}).catch(err => logger.info(err));
+}).catch(err => logger.info(err.stack));
 
 module.exports = db;
