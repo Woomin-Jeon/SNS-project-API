5be74a7d2c550957fb04c26f9d58985ec4a5238c
woomin <dal96k@gmail.com>
2020-04-17 12:33:52 +0900

    Change file upload path. Frontend -> api

--- routes/upload.js
+++ routes/upload.js
@@ -7,10 +7,10 @@ const wrapAsync = require('../middleware/wrapAsync');
 router.post('/', wrapAsync(async (req, res) => {
   const key = await method.getKey();
   const file = req.files.file;
-  const filePath = `${__dirname}/../../FrontEnd/img/${key}${file.name}`;
+  const filePath = `${__dirname}/../img/${key}${file.name}`;
 
   file.mv(filePath, () => {
-    res.json({ fileName: file.name, filePath: `/img/${key}${file.name}` });
+    res.json({ fileName: file.name, filePath: `https://woomin-facebook.herokuapp.com/img/${key}${file.name}` });
   });
 }));
 
