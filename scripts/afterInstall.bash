b481c1edad8ad031023dfdb4f511affdcc237ece
woomin <dal96k@gmail.com>
2020-05-18 18:35:24 +0900

    Modify env script

--- scripts/afterInstall.bash
+++ scripts/afterInstall.bash
@@ -1,2 +1,2 @@
 docker pull dal96k/woomin-facebook:latest
-docker run --env-file <(env | grep -vE '\r|\n' | grep -iE 'MONGODB_URI|DB_DEVELOPMENT|DB_TEST|SENTRY_DSN|AWS_ACCESS_KEY|AWS_SECRET_ACCESS_KEY') \ /home/ubuntu/.env --publish 7070:7070 -it --detach --name woomin-facebook-codedeploy dal96k/woomin-facebook:latest /bin/bash
+docker run --env-file /home/ubuntu/.env --publish 7070:7070 -it --detach --name woomin-facebook-codedeploy dal96k/woomin-facebook:latest /bin/bash
