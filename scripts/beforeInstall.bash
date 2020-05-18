5c8c31ffbc2067aa0bd70aeeec56ce19c662d831
woomin <dal96k@gmail.com>
2020-05-18 17:14:22 +0900

    Modify appspec file

--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -4,6 +4,6 @@ fi
 mkdir -vp /home/ubuntu/build
 
 
-if [[ - "$(docker images -q dal96k/woomin-facebook-codedeploy:latest 2> /dev/null)" != "" ]]; then
+if [[ "$(docker images -q dal96k/woomin-facebook-codedeploy:latest 2> /dev/null)" != "" ]]; then
 docker rmi -f $(docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference='dal96k/woomin-facebook-codedeploy:latest')
 fi
