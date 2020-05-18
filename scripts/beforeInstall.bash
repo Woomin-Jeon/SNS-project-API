2093515236efe3c5bcf8f076a407eaa1c3910eee
woomin <dal96k@gmail.com>
2020-05-18 20:24:24 +0900

    Modify beforeInstall.bash

--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -4,6 +4,7 @@ fi
 mkdir -vp /home/ubuntu/build
 
 docker stop woomin-facebook-codedeploy
+docker rm woomin-facebook-codedeploy
 
 if [[ "$(docker images -q dal96k/woomin-facebook:latest 2> /dev/null)" != "" ]]; then
 docker rmi -f $(docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference='dal96k/woomin-facebook:latest')
