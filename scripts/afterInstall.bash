a9161cbbe97c3cadccb1e96c148d1b65586037da
woomin <dal96k@gmail.com>
2020-05-18 18:07:22 +0900

    Modify deploying logic > pull image from dockerHub

--- scripts/afterInstall.bash
+++ scripts/afterInstall.bash
@@ -1,5 +1,2 @@
-cp /home/ubuntu/.env /home/ubuntu/build/
-cd /home/ubuntu/build
-npm install
-docker build -t dal96k/woomin-facebook-codedeploy-image:latest .
-docker run --publish 7070:7070 -it --detach --name woomin-facebook-codedeploy dal96k/woomin-facebook-codedeploy-image:latest /bin/bash
+docker pull dal96k/woomin-facebook:latest
+docker run --env-file /home/ubuntu/.env --publish 7070:7070 -it --detach --name woomin-facebook-codedeploy dal96k/woomin-facebook:latest /bin/bash
--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -5,6 +5,6 @@ mkdir -vp /home/ubuntu/build
 
 docker stop woomin-facebook-codedeploy
 
-if [[ "$(docker images -q dal96k/woomin-facebook-codedeploy-image:latest 2> /dev/null)" != "" ]]; then
-docker rmi -f $(docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference='dal96k/woomin-facebook-codedeploy-image:latest')
+if [[ "$(docker images -q dal96k/woomin-facebook:latest 2> /dev/null)" != "" ]]; then
+docker rmi -f $(docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference='dal96k/woomin-facebook:latest')
 fi
