992ea78fcb63d12cebdae21211567bd9bd799411
woomin <dal96k@gmail.com>
2020-05-18 17:44:09 +0900

    Modify afterInstall file

--- scripts/afterInstall.bash
+++ scripts/afterInstall.bash
@@ -1,5 +1,5 @@
 cp /home/ubuntu/.env /home/ubuntu/build/
 cd /home/ubuntu/build
 npm install
-docker build -t dal96k/woomin-facebook-codedeploy:latest .
+docker build -t dal96k/woomin-facebook-codedeploy-image:latest .
 docker run --publish 7070:7070 -it --detach --name woomin-facebook-codedeploy dal96k/woomin-facebook-codedeploy-image:latest /bin/bash
