6f11e30bfc89c6b2b338b83f2c26e483f07a9ea7
woomin <dal96k@gmail.com>
2020-05-18 17:09:55 +0900

    Modify beforeInstall file

--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -1,9 +1,9 @@
-if [ -d /home/ubuntu/build ]; then
+- if [ -d /home/ubuntu/build ]; then
     rm -rf /home/ubuntu/build
 fi
 mkdir -vp /home/ubuntu/build
 
 
-if [[ "$(docker images -q dal96k/woomin-facebook-codedeploy:latest 2> /dev/null)" != "" ]]; then
+- if [[ "$(docker images -q dal96k/woomin-facebook-codedeploy:latest 2> /dev/null)" != "" ]]; then
 docker rmi -f $(docker images --format '{{.Repository}}:{{.Tag}}' --filter=reference='dal96k/woomin-facebook-codedeploy:latest')
 fi
